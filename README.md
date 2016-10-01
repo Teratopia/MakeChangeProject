7094a6db6635196d9384c5175d0a848cdcb353f1
Teratopia <kylewoodennis@gmail.com>
2016-10-01 12:15:07 -0600

    Added loop and exit conditions for multiple transactions

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 ##Make Change Project
+
+
+To run the program the user must first input the cost of the item, then press enter.
+Next the user must input the amount of cash used to pay for the item, then press enter.
+
+The program will calculate the change left over from the transaction, then print
+how many of each bill should be returned.
+
+To close the program enter a negative number for either input.
--- src/cashregister/MakeChange.java
+++ src/cashregister/MakeChange.java
@@ -2,6 +2,7 @@ package cashregister;
 
 public class MakeChange {
 	public static void main(String[] args) {
+		boolean isRunning = true;
 		int[] register = { 2000, 1000, 500, 100, 25, 10, 5, 1 };
 		int[] cashBack = new int[8];
 		String[][] plurals = { { "twenty dollar bill", "twenty dollar bills" },
@@ -9,34 +10,49 @@ public class MakeChange {
 				{ "one dollar bill", "one dollar bills" }, { "quarter", "quarters" }, { "dime", "dimes" },
 				{ "nickel", "nickels" }, { "penny", "pennies" }, };
 
+		System.out.println("\nThis is a cash register program. To exit, enter a negative number.\n");
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
-		System.out.print("Please enter the cost of the item: ");
-		double cost = scanner.nextDouble() * 100;
-		System.out.print("Please enter cash recieved: ");
-		double cash = scanner.nextDouble() * 100;
-		int iCents = (int) (cash - cost);
-		scanner.close();
+		
+		while (isRunning == true) {
+			System.out.print("Please enter the cost of the item: ");
+			double cost = scanner.nextDouble() * 100;
+			if(cost < 0){
+				System.out.println("\nGoodbye");
+				isRunning=false;
+				break;
+			}
+			System.out.print("Please enter cash recieved: ");
+			double cash = scanner.nextDouble() * 100;
+			if(cash < 0){
+				System.out.println("\nGoodbye");
+				isRunning=false;
+				break;
+			}
+			
+			int iCents = (int) (cash - cost);			
 
-		if (iCents < 0) {
-			System.out.println("That is not enough money.");
-		} else {
-			System.out.print("Your change is ");
-			for (int i = 0; iCents > 0; i++) {
-				cashBack[i] = iCents / register[i];
-				iCents = iCents % register[i];
+			if (iCents < 0) {
+				System.out.println("That is not enough money.");
+			} else {
+				System.out.print("\nYour change is ");
+				for (int i = 0; iCents > 0; i++) {
+					cashBack[i] = iCents / register[i];
+					iCents = iCents % register[i];
 
-				if (cashBack[i] == 1) {
-					System.out.print(cashBack[i] + " " + plurals[i][0]);
-				} else if (cashBack[i] > 1) {
-					System.out.print(cashBack[i] + " " + plurals[i][1]);
-				}
+					if (cashBack[i] == 1) {
+						System.out.print(cashBack[i] + " " + plurals[i][0]);
+					} else if (cashBack[i] > 1) {
+						System.out.print(cashBack[i] + " " + plurals[i][1]);
+					}
 
-				if (cashBack[i] > 0 && iCents == 0) {
-					System.out.println(".");
-				} else if (cashBack[i] > 0) {
-					System.out.print(", ");
+					if (cashBack[i] > 0 && iCents == 0) {
+						System.out.println(".\n");
+					} else if (cashBack[i] > 0) {
+						System.out.print(", ");
+					}
 				}
 			}
 		}
+		scanner.close();
 	}
 }
