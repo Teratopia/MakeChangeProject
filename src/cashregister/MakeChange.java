e8e1f1c30e3a52a2c73e517917cad4a94d593074
Teratopia <kylewoodennis@gmail.com>
2016-10-01 11:46:02 -0600

    Should be complete now

--- src/cashregister/MakeChange.java
+++ src/cashregister/MakeChange.java
@@ -4,46 +4,39 @@ public class MakeChange {
 	public static void main(String[] args) {
 		int[] register = { 2000, 1000, 500, 100, 25, 10, 5, 1 };
 		int[] cashBack = new int[8];
-		String[][] plurals = {
-				{"twenty dollar bill","twenty dollar bills"} , {"ten dollar bill","ten dollar bills"} ,
-				{"five dollar bill","five dollar bills"} , {"one dollar bill","one dollar bills"} ,
-				{"quarter","quarters"} , {"dime","dimes"} , {"nickel","nickels"} , {"penny","pennies"} , 
-		};
-		
+		String[][] plurals = { { "twenty dollar bill", "twenty dollar bills" },
+				{ "ten dollar bill", "ten dollar bills" }, { "five dollar bill", "five dollar bills" },
+				{ "one dollar bill", "one dollar bills" }, { "quarter", "quarters" }, { "dime", "dimes" },
+				{ "nickel", "nickels" }, { "penny", "pennies" }, };
+
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
 		System.out.print("Please enter the cost of the item: ");
-		double cost = scanner.nextDouble()*100;
+		double cost = scanner.nextDouble() * 100;
 		System.out.print("Please enter cash recieved: ");
-		double cash = scanner.nextDouble()*100;
-		int iCents = (int)(cash-cost);
+		double cash = scanner.nextDouble() * 100;
+		int iCents = (int) (cash - cost);
 		scanner.close();
-	
-		if(iCents<0){
+
+		if (iCents < 0) {
 			System.out.println("That is not enough money.");
-		}else{
+		} else {
 			System.out.print("Your change is ");
-			for(int i = 0 ; iCents>0 ; i++){
-				cashBack[i] = iCents/register[i];
+			for (int i = 0; iCents > 0; i++) {
+				cashBack[i] = iCents / register[i];
 				iCents = iCents % register[i];
-				
-				if(cashBack[i]==1){
-					System.out.print(cashBack[i]+" "+plurals[i][0]);
-				}else if(cashBack[i]>1){
-					System.out.print(cashBack[i]+" "+plurals[i][1]);
+
+				if (cashBack[i] == 1) {
+					System.out.print(cashBack[i] + " " + plurals[i][0]);
+				} else if (cashBack[i] > 1) {
+					System.out.print(cashBack[i] + " " + plurals[i][1]);
 				}
-				
-				if(iCents==0 && cashBack[i]==0){
-				System.out.print(".");
-				}else{
+
+				if (cashBack[i] > 0 && iCents == 0) {
+					System.out.println(".");
+				} else if (cashBack[i] > 0) {
 					System.out.print(", ");
 				}
 			}
-			System.out.println("\b\b.");
 		}
-		
-		
-		
-		
 	}
-
 }
